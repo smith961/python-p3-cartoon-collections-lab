ab64cf3fb0de3ac8d603d744a05b43633752d0b3
smith <smithbabawande@gmail.com>
2023-10-03 23:59:41 +0100

    Completed Assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    i = 1
+    for name in names:
+        print(f"{i}. {name}")
+        i += 1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    new_list = []
+    for call in planeteer_calls:
+        new_list.append(call.capitalize() + "!")
+    return new_list
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
+
+
+def find_the_cheese(foods):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in cheese_types:
+            return food
+    return None
